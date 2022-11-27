b97f261a4c578f8134e893db487d41b48be378b8
martinchristensen <martinchristensen1995@gmail.com>
2022-11-27 02:40:32 +0000

    Deploying to main from  @ 237bac3567af9b34df021e590bad97d0153211ba 🚀

--- types.ts
+++
@@ -1,7 +0,0 @@
-export interface selectableItem {
-    id: number;
-    value: string;
-    element: JSX.Element;
-  }
-
-  export type selectableColors = '#8B2424'|'#35DEE4'|'#1199F4'|'#B4171C'
--- utils/constants.ts
+++
@@ -1,6 +0,0 @@
-import { selectableColors } from "../types";
-
-export const RED: selectableColors = "#B4171C";
-export const DARK_RED: selectableColors = "#8B2424";
-export const CYAN: selectableColors = "#35DEE4";
-export const BLUE: selectableColors = "#1199F4";
